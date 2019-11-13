899d96b1591c44af78c78f8be8514853dc7c2d9c
danipem <56585156+danipem@users.noreply.github.com>
2019-11-13 11:25:28 +0100

    practica

---
+++ NVL-JS-P8a.js
@@ -0,0 +1,10 @@
+var a = "Amigo"
+
+//pasando todo a minúsculas
+alert(a.toLowerCase())
+
+//sustituyendo la primera letra
+alert(a.replace("A", "a"))
+
+//cogiendo solamente desde la segunda letra más "a"
+alert("a" + a.slice(1,5))
